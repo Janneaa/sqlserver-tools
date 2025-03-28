183be3d8cab33d83f3c611107e01fa4ca55003bc
Janneaa <janneaa@gmail.com>
2025-03-28 12:51:19 +0200

    Update get_schema_owners.sql

--- Permissions/get_schema_owners.sql
+++ Permissions/get_schema_owners.sql
@@ -2,6 +2,8 @@ SELECT s.Name as SchemaName, u.name AS [Owner]
 FROM sys.schemas s
 INNER JOIN sys.sysusers u
     ON u.uid = s.principal_id
+INNER JOIN sys.database_principals dp
+    ON s.principal_id = dp.principal_id
 WHERE s.Name NOT IN ('dbo', 'guest', 'sys', 'INFORMATION_SCHEMA')
-	AND s.Name NOT LIKE 'db[_]%'
+    AND dp.type = 'S'
 ORDER BY s.Name, u.name
